8c070d3c806ed322065c33c021c028bccce8e054
Veljko Djordjevic <veljkodjordjevic97@gmail.com>
2020-12-07 13:12:50 +0100

    added stopwatch

---
+++ stopwatch/DE0_TOP/my_top.v
@@ -0,0 +1,72 @@
+module my_top
+	#(
+		parameter SECONDS_WIDTH = 10,
+		parameter NUM_OF_7SEGS = 4
+	)
+	(
+		input async_reset,
+		input clk,
+		input start,
+		input continue_pause,
+		output [(SECONDS_WIDTH - 1) : 0] seconds_passed_led,
+		output [(NUM_OF_7SEGS * 8 - 1) : 0] seconds_passed_7segs
+	);
+	
+	wire continue_pause_red;
+	
+	rising_edge_detector
+	(
+		.async_reset(async_reset),
+		.clk(clk),
+		.signal_input(continue_pause),
+		.signal_output(continue_pause_red)
+	);
+	
+	wire [(SECONDS_WIDTH - 1) : 0] stopwatch_instance_seconds_passed;
+	
+	stopwatch
+		# (
+			.SECONDS_WIDTH(SECONDS_WIDTH)
+		)
+	stopwatch_instance
+		(
+			.async_reset(async_reset),
+			.clk(clk),
+			.start(start),
+			.continue_pause(continue_pause_red),
+			.seconds_passed(stopwatch_instance_seconds_passed)
+		);
+		
+	assign seconds_passed_led = stopwatch_instance_seconds_passed;
+		
+	genvar i;
+	generate
+		for (i = 0; i < NUM_OF_7SEGS; i = i + 1) begin : generate_block
+			wire [3 : 0] digit = (stopwatch_instance_seconds_passed / (10 ** i)) % 10;
+			wire [7 : 0] digit_seven_seg = sevenseg(digit);
+			assign seconds_passed_7segs[8 * (i+1) - 1 : 8 * i] = digit_seven_seg;
+		end
+	endgenerate
+	
+	
+	function [7 : 0] sevenseg
+	(
+		input [3 : 0] digit
+	);
+	begin
+		case (digit)
+			4'b0000: sevenseg = 8'hC0;
+			4'b0001: sevenseg = 8'hF9;
+			4'b0010: sevenseg = 8'hA4;
+			4'b0011: sevenseg = 8'hB0;
+			4'b0100: sevenseg = 8'h99;
+			4'b0101: sevenseg = 8'h92;
+			4'b0110: sevenseg = 8'h82;
+			4'b0111: sevenseg = 8'hF8;
+			4'b1000: sevenseg = 8'h80;
+			4'b1001: sevenseg = 8'h90;
+		endcase
+	end
+	endfunction
+	
+endmodule 
---
+++ stopwatch/DE0_TOP/stopwatch.v
@@ -0,0 +1,91 @@
+`include "register.vh"
+
+module stopwatch
+	#(
+		parameter SECONDS_WIDTH = 8
+	)
+	(
+		input async_reset,
+		input clk,
+		input start,
+		input continue_pause,
+		output reg [(SECONDS_WIDTH - 1) : 0] seconds_passed
+	);
+	
+	localparam one_second = 28'h2FAF080;
+	
+	reg [2 : 0] cntr_ctrl;
+	wire [27 : 0] cntr_data_output;
+	
+	register
+		#(
+			.WIDTH(28)
+		)
+	cntr
+		(
+			.async_reset(async_reset),
+			.clk(clk),
+			.ctrl(cntr_ctrl),
+			.data_input(28'h000_0000),
+			.data_output(cntr_data_output)
+		);
+	
+	reg [2 : 0] seconds_ctrl;
+	wire [(SECONDS_WIDTH - 1) : 0] seconds_data_output;
+	
+	register
+		#(
+			.WIDTH(SECONDS_WIDTH)
+		)
+	seconds
+		(
+			.async_reset(async_reset),
+			.clk(clk),
+			.ctrl(seconds_ctrl),
+			.data_input({SECONDS_WIDTH{1'b0}}),
+			.data_output(seconds_data_output)
+		);
+	
+	localparam WAITING  = 2'b00;
+	localparam COUNTING = 2'b01;
+	localparam PAUSED   = 2'b10;
+	
+	reg [1 : 0] state_reg, state_next;
+	
+	always @(negedge async_reset, posedge clk) begin
+		if (!async_reset)
+			state_reg <= 2'b0;
+		else
+			state_reg <= state_next;
+	end
+	
+	always @(*) begin
+	
+		cntr_ctrl <= `REG_CTRL_NOP;
+		seconds_ctrl <= `REG_CTRL_NOP;
+		state_next <= state_reg;
+		seconds_passed <= seconds_data_output;
+		
+		case (state_reg)
+			WAITING: begin
+				if (start == 1'b1)
+					state_next <= COUNTING;
+			end
+			COUNTING: begin
+				cntr_ctrl <= `REG_CTRL_INC;
+				if (cntr_data_output == one_second) begin
+					cntr_ctrl <= `REG_CTRL_CLR;
+					seconds_ctrl <= `REG_CTRL_INC;
+				end
+				if (continue_pause == 1'b1)
+					state_next <= PAUSED;
+			end
+			PAUSED: begin
+				if (continue_pause == 1'b1)
+					state_next <= COUNTING;
+			end
+		endcase
+	
+	end
+	
+endmodule
